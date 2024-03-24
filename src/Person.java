7a19a3f27eea17d3e400c8cd7384d1d6fe84bb67
1 <menathlakvindub@gmail.com>
2024-03-24 14:01:48 +0530

    feat(test) - Updated process inside main class

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,35 @@
 public class Person {
     private String name;
+    private String surname;
     private String email;
 
-    public Person(String name, String email) {
+    public Person(String name, String surname, String email) {
         this.name = name;
+        this.surname = surname;
         this.email = email;
     }
+    public void set_name(String name) {
+        this.name = name;
+    }
+    public String get_name() {
+        return name;
+    }
+    public void set_surname(String surname) {
+        this.surname = surname;
+    }
 
-    public void set_name(String name) { this.name = name; }
-    public String get_name() { return name; }
-
-    public void set_email(String email) { this.email = email; }
-    public String get_email() { return email; }
-
-
+    public String get_surname() {
+        return surname;
+    }
+    public void set_email(String email) {
+        this.email = email;
+    }
+    public String get_email() {
+        return email;
+    }
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Email: " + email);
+    }
 }
